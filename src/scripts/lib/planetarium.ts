ff3877138b67bdef29f704a8af384c1ae150953b
Jaime Sánchez <jsanchez@surcontrol.com>
2024-03-19 21:00:33 +0100

    Añadidas luces de noche y día

--- src/scripts/lib/planetarium.ts
+++ src/scripts/lib/planetarium.ts
@@ -14,12 +14,12 @@ export default class Planetarium {
     textureLoader: THREE.TextureLoader;
 
     earthGeometry: THREE.SphereGeometry;
-    earthMaterial: THREE.MeshLambertMaterial;
+    earthMaterial: THREE.MeshStandardMaterial;
     earthTexture: THREE.Texture;
     earth: THREE.Mesh;
 
     issGeometry: THREE.SphereGeometry;
-    issMaterial: THREE.MeshBasicMaterial;
+    issMaterial: THREE.MeshStandardMaterial;
     iss: THREE.Mesh;
 
     earthRadius: number = 8; // radio de la Tierra en metros
@@ -27,11 +27,14 @@ export default class Planetarium {
 
     issAltitude: number = 2;
 
+    dayLight: THREE.DirectionalLight;
+    nightLight: THREE.DirectionalLight;
+
     constructor(initialLat: number, initialLong: number) {
         this.startAnimation = this.startAnimation.bind(this);
 
         this.scene = new THREE.Scene();
-        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        this.camera = new THREE.PerspectiveCamera(120, window.innerWidth / window.innerHeight, 0.1, 1000);
         this.camera.position.z = 20;
         this.renderer = new THREE.WebGLRenderer();
 
@@ -40,22 +43,33 @@ export default class Planetarium {
         this.controls.dampingFactor = 0.05;
 
         this.ambientalLight = new THREE.AmbientLight( 0xffffff );
-        this.scene.add(this.ambientalLight);
+        // this.scene.add(this.ambientalLight);
+
+        this.dayLight = new THREE.DirectionalLight(0xffffff, 1);
+        this.dayLight.position.set(1, 1, 1).normalize();
+
+        this.nightLight = new THREE.DirectionalLight(0xffffff, 0.1);
+        this.nightLight.position.set(-1, -1, -1).normalize();
+
+        this.scene.add(this.dayLight, this.nightLight);
+
+        const helper = new THREE.DirectionalLightHelper( this.dayLight, 15);
+        // this.scene.add( helper );
 
         // The X axis is red. The Y axis is green. The Z axis is blue. (https://threejs.org/docs/?q=axes%20helper#api/en/helpers/AxesHelper)
         const axesHelper = new THREE.AxesHelper(100);
-        this.scene.add(axesHelper);
+        // this.scene.add(axesHelper);
 
         this.textureLoader = new THREE.TextureLoader();
         this.earthTexture = this.textureLoader.load("./earth-texture.jpg");
 
         this.earthGeometry = new THREE.SphereGeometry(this.earthRadius, 150, 150);
-        this.earthMaterial = new THREE.MeshLambertMaterial({ map: this.earthTexture });
+        this.earthMaterial = new THREE.MeshStandardMaterial({ map: this.earthTexture });
         this.earth = new THREE.Mesh(this.earthGeometry, this.earthMaterial)
         this.scene.add(this.earth);
 
         this.issGeometry = new THREE.SphereGeometry(this.issRadius, 32, 32);
-        this.issMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
+        this.issMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff });
         this.iss = new THREE.Mesh(this.issGeometry, this.issMaterial);
         this.scene.add(this.iss);
 
@@ -66,7 +80,6 @@ export default class Planetarium {
 
         this.startAnimation();
     }
-
 
     startAnimation() {
         requestAnimationFrame(this.startAnimation);
